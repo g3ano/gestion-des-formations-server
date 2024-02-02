123ca5102169614c97c4a410425d583a0d8c2be7
ghano <ghanoasx@gmail.com>
2024-02-02 01:42:56 +0100

    style: remo a comment

--- routes/v1/formation.php
+++ routes/v1/formation.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\v1\FormationController;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:sanctum')->group(function () {
-// });
-
 Route::prefix('formations')->group(function () {
     Route::get('/', [FormationController::class, 'index']);
     Route::post('/', [FormationController::class, 'store']);
